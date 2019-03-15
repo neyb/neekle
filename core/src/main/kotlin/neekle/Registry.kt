ab4f099c9e261a91378ecf6aed7748ab30786902
neyb <noo.blaster@gmail.com>
2019-03-15 08:04:20 +0100

    tmp

--- core/src/main/kotlin/neekle/Registry.kt
+++ core/src/main/kotlin/neekle/Registry.kt
@@ -1,7 +1,22 @@
 package neekle
 
-class Registry() {
+import neekle.inject.api.Locator
+
+internal class Registry : BindingsFinder {
     private val bindings = mutableListOf<Binding<*>>()
-    private val modules = mutableListOf<Module>()
+    private val locators = mutableListOf<Locator>()
+
+    fun <T> add(binding: Binding<T>) {
+        bindings.add(binding)
+    }
+
+    fun add(locator: Locator) {
+        locators += locator
+    }
+
+    override fun <T> getBindings(definition: BindingDefinition<T>): List<Binding<T>> {
+        return bindings.mapNotNull { it.asCandidateForOrNull(definition) }
+    }
+
 
 }
